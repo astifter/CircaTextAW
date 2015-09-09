a1eb7f79701451d62f422f976eb96352408ad0a6
Andreas Neustifter <andreas.neustifter@gmail.com>
2015-09-09 17:10:37 +0200

    Use onDraw interface for text.

---
+++ Wearable/src/main/java/com/astifter/circatext/graphicshelpers/CircaTextDrawable.java
@@ -0,0 +1,12 @@
+package com.astifter.circatext.graphicshelpers;
+
+import android.graphics.Canvas;
+import android.graphics.Rect;
+
+/**
+ * Created by astifter on 09.09.15.
+ */
+public interface CircaTextDrawable {
+    void onDraw(Canvas canvas, Rect bounds);
+}
+
