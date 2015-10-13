e7e6fc62247578c479ea9359c229041720c4f101
Andreas Neustifter <andreas.neustifter@gmail.com>
2015-10-13 21:06:25 +0200

    Added original SVG files.

---
+++ Wearable/src/main/java/com/astifter/circatext/graphicshelpers/AnimatableIcon.java
@@ -0,0 +1,12 @@
+package com.astifter.circatext.graphicshelpers;
+
+import android.support.wearable.watchface.CanvasWatchFaceService;
+
+/**
+ * Created by astifter on 13.10.15.
+ */
+public class AnimatableIcon extends AnimatableImpl implements Animatable {
+    AnimatableIcon(CanvasWatchFaceService.Engine p, AnimatableIcon d) {
+        super(p, d);
+    }
+}
---
+++ Wearable/src/main/java/com/astifter/circatext/graphicshelpers/DrawableIcon.java
@@ -0,0 +1,7 @@
+package com.astifter.circatext.graphicshelpers;
+
+/**
+ * Created by astifter on 13.10.15.
+ */
+public class DrawableIcon {
+}
